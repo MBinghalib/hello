495d03d23f24e8d38ec7f2b30d4e4ee5b2fadbfb
Mariam Binghalib <mmbinghalib@gmail.com>
2021-01-28 10:23:01 +0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import "./styles.css";
+
+export default function App() {
+  return (
+    <form>
+    <label>
+      Name:
+      <input type="text" name="name" /> 
+      <br/> <br/>
+      Age: 
+      <input type="number" id="quantity" name="quantity" min="1" max="100"/>
+    </label>
+    <br/> <br/>
+    <input type="submit" value="Submit" />
+  </form>
+  );
+}
